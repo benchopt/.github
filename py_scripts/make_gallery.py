bffd68f556c3888d8eb760c64013bdb0eca5bd68
Badr MOUFAD <Badr.MOUFAD@emines.um6p.ma>
2022-06-17 18:32:02 +0200

    reproduce benchmark explanantion

--- py_scripts/make_gallery.py
+++ py_scripts/make_gallery.py
@@ -26,7 +26,37 @@ def _process_description(description):
 
 if __name__ == "__main__":
     g = Github().get_user('benchopt')
-    content = "# Gallery of Benchmarks\n\n"
+    content = """# Benchmarks gallery
+
+    To reproduce / modify a benchmark,
+    
+    1. clone the benchmark repository and ``cd`` into it,
+
+    ```shell
+    $ git clone https://github.com/benchopt/{REPOSITORY_NAME}.git
+    $ cd {REPOSITORY_NAME}
+    ```
+
+    2. install ``benchopt`` to use its API/CLI,
+
+    ```shell
+    $ pip install -U benchopt
+    ```
+    
+    3. run the following command to reproduce the benchmark,
+
+    ```shell
+    $ benchopt run .
+    ```
+
+    Et Voilà! 
+    
+    Refer to [``Benchopt`` API](https://benchopt.github.io/cli.html#benchopt-run)
+    to explore the different options for running a benchmark.
+
+
+    ### Available benchmarks\n\n
+    """
 
     for repo in g.get_repos():
         if repo.name.startswith('benchmark'):
